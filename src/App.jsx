da6bdf5876df4b613bbf07f3738722bb94ac2abd
Tej Magar <magartejofficial@gmail.com>
2024-03-02 21:54:31 +0545

    useEffect used, install usehooks for local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Filter from "./Comonents/Filter";
 import Form from "./Comonents/Form";
 import Header from "./Comonents/Header";
 import Todo from "./Comonents/Todo";
+import { useLocalStorage } from "usehooks-ts";
 
 const App = () => {
   const [appState, setAppState] = useState();
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: "Complete online JavaScript course",
-      completed: true,
-    },
+  const [todos, setTodos] = useLocalStorage("todos",[
+    // {
+    //   id: 1,
+    //   title: "Complete online JavaScript course",
+    //   completed: true,
+    // },
   ]);
+
+
+  const [filterTodos, setFilterTodos] = useState(todos);
+
+  useEffect(() => setFilterTodos(todos), [todos]);
+
   return (
     <div className="min-h-screen w-full bg-stone-900">
       <div className="mx-auto min-h-screen max-w-md bg-stone-950">
         <Header></Header>
         <main className="bg-stone-950 p-5">
           <Form todos={todos} setTodos={setTodos}></Form>
-          <Filter></Filter>
+          <Filter todos={todos} setFilterTodos={setFilterTodos}></Filter>
           <div>
-            {todos.map((todo) => (
+            {filterTodos.map((todo) => (
               <Todo key={todo.id} setTodos={setTodos} todo={todo} />
             ))}
-           
           </div>
         </main>
       </div>
--- src/Comonents/Filter.jsx
+++ src/Comonents/Filter.jsx
@@ -1,12 +1,20 @@
-const Filter = () => {
+const Filter = ({ todos, setFilterTodos }) => {
   return (
     <div className="my-3 flex items-center justify-between text-white">
       <h2 className="text-xl font-bold">YOUR TODOS</h2>
       <div className="space-x-3">
-        <button className="h-10 rounded bg-stone-600 px-3 text-white">
+        <button
+          className="h-10 rounded bg-stone-600 px-3 text-white"
+          onClick={() => setFilterTodos(todos)}
+        >
           All
         </button>
-        <button className="h-10 rounded bg-stone-600 px-3 text-white">
+        <button
+          className="h-10 rounded bg-stone-600 px-3 text-white"
+          onClick={() =>
+            setFilterTodos(todos.filter((todo) => !todo.completed))
+          }
+        >
           Actives
         </button>
       </div>
